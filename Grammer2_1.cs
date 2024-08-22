bbdf039f5c1fc19c2126658f82dc8fa1e5c5137f
HyunSeok Suh <suhya990312@gmail.com>
2024-08-22 23:44:58 +0900

    commitG

---
+++ Grammer2_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grammer
+{
+    class Grammer2_1
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            for(int i=num; i>0; i--)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("Factorial of {0} is {1}", num, factorial);
+        }
+    }
+}
