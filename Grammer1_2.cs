ddf1a6414be8d61a7441cd06bc7d1cd903f08424
HyunSeok Suh <suhya990312@gmail.com>
2024-08-22 23:40:04 +0900

    commitG

---
+++ Grammer1_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grammer
+{
+    class Grammer1_2
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            float avarage = 0;
+
+            string[] str = Console.ReadLine().Split(' ');
+
+            for(int i=0; i<str.Length; i++)
+            {
+                sum += int.Parse(str[i]);
+            }
+
+            avarage = (float)sum / str.Length;
+
+            Console.WriteLine("Sum : " + sum);
+            Console.WriteLine("Avarage : " + avarage);
+        }
+    }
+}
