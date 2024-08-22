971f90a1ea66e542d141f2a671216af427365e20
HyunSeok Suh <suhya990312@gmail.com>
2024-08-22 17:23:59 +0900

    commitG

---
+++ Grammer1_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Grammer
+{
+    class Grammer1_1
+    {
+        static void Main(string[] args)
+        {
+            /*for문을 이용
+            for(int i=1; i<100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*while문 이용
+            int i = 1;
+            while(i<100)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+            */
+
+            //do while문 이용
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i += 2;
+            } while (i < 100);
+        }
+    }
+}
